64aa0717b78ba66c7304f6a4080e793466f3c8a2
Scott <mabezdev@gmail.com>
2017-09-26 20:28:05 +0100

    [i3 Status] Added Ranger FM, added custom lockscreen, added scripts folder in i3 folder

---
+++ i3/.config/i3/scripts/customlock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+i3lock -k --blur 6 --timecolor=FFFFFFD0 \
+	--datecolor=FFFFFFD0 \
+	--insidecolor=FFFFFF00 \
+	--ringcolor=4a2c5bFF \
+	--keyhlcolor=2a1c33FF \
+	--insidevercolor=FFFFFF00 \
+	--ringvercolor=4a2c5bFF
