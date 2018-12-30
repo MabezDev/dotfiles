5a4b150124cc8cda6b62ed5b637b6b720d3148cc
Scott Mabin <MabezDev@gmail.com>
2018-12-30 01:26:45 +0000

    Numerous polybar changes, including tray output with primary monitor detection. Added breeze-dark cursor theme

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,9 +10,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #polybar main &
 #polybar bar2 &
 
+PRIMARY=$(xrandr --query | grep " primary" | cut -d" " -f1)
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    if [ "$m" == "$PRIMARY" ]; then
+      TRAY_POS="right" MONITOR=$m polybar --reload main &
+    else
+      MONITOR=$m polybar --reload main &
+    fi
   done
 else
   polybar --reload main &
