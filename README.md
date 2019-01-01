5c639d338afa799e7d8f0b4eb00903b5192aeca1
Scott Mabin <MabezDev@gmail.com>
2019-01-01 22:43:33 +0000

    Improved custom lock, Added more info in readmen about more advanced setups

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Installation
 
-# Deps
+## Deps
  - GNU stow
  - oh-my-zsh
 
-# Steps
+Install deps and run `git clone --recursive .dotfiles` in home directory. Then run './install {type}' inside of `.dotfiles` where `type` can be `laptop` or `desktop`.
 
-Install deps and run `git clone --recursive .dotfiles` in home directory. Then run 'stow *' inside of `.dotfiles` 
+## Example xinitrc
+
+```
+xrdb merge $HOME/.Xresources   
+/usr/bin/i3lock -e -u -i /home/mabez/.config/i3/icons/screen_lock_boot.png
+/usr/bin/xautolock -detectsleep -time 5 -locker '/home/mabez/.config/i3/scripts/customlock.sh' &
+exec ck-launch-session dbus-launch --sh-syntax --exit-with-session i3
+```
+
+This will merge Xresources, launch te initial lock screen for login
+then set an autolock for 5minutes of afk, finally launch i3 with a consolekit session
+
+## Detecting lid closing/suspend
+
+`/etc/acpid/handler.sh` will handle all acpi events, find the button/lid case and append your lock code before `zzz` is invoked.
+
--- i3/.config/i3/scripts/customlock.sh
+++ i3/.config/i3/scripts/customlock.sh
@@ -1,43 +1,27 @@
 #!/usr/bin/env bash
 
+while getopts ":i:" o; do
+    case "${o}" in
+        i)
+            i=${OPTARG}
+            ;;
+    esac
+done
+
+lock=/home/mabez/.config/i3/icons/locked-padlock_256x256.png
 image_file=/tmp/screen_lock.png
 image_file_final=/tmp/screen_lock_final.png
 rm $image_file
 rm $image_file_final
 
-resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
-# filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor' 
+resolution=$(xdpyinfo | grep dimensions | awk '{print $2}') 
 filters='boxblur=luma_radius=min(h\,w)/100'
-# lock_overlay='-i /home/mabez/.config/i3/icons/circlelockcleardark.png -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2"'
 
-
-ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
-  -vf "$filters" "$image_file"
-
-ffmpeg -y -loglevel 0 -i $image_file -i /home/mabez/.config/i3/icons/locked-padlock.png -filter_complex \
-"[1:v]scale=256:256 [ovrl], \
-[0:v][ovrl]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
-$image_file_final
-
-# ffmpeg -y -s "$resolution" -i "$image_file" $lock_overlay  "$image_file"
+if [[ -z "$i" ]]; then
+  ffmpeg -f x11grab -video_size $resolution -y -i $DISPLAY -i $lock -filter_complex "$filters,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $image_file_final
+else
+  cp $i /tmp/input.png
+  ffmpeg -y -i /tmp/input.png -i $lock -filter_complex "$filters,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $image_file_final
+fi
 
 i3lock -e -u -i "$image_file_final"
-
-### OLD, slow with convert
-
-# set the icon and a temporary location for the screenshot to be stored
-# icon="$HOME/images/lock-icon-light.png"
-# tmpbg='/tmp/screen.png'
-
-# # take a screenshot
-# scrot "$tmpbg"
-
-# # blur the screenshot by resizing and scaling back up
-# # convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
-# mogrify "$tmpbg" -filter Gaussian -resize %20 -define "filter:signma=1.5" -resize 500.5% "$tmpbg"
-
-# # overlay the icon onto the screenshot
-# # convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
-
-# # lock the screen with the blurred screenshot
-# i3lock -i "$tmpbg" -u
