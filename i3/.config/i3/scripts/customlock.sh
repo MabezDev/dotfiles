9b93fcf4176bf8ea3eca3617ce64a0bdc953bee9
Scott Mabin <MabezDev@gmail.com>
2019-01-01 03:22:55 +0000

    Added custom lock that uses ffmpeg, added thermal zones for laptop and wifi. i3lock

--- i3/.config/i3/scripts/customlock.sh
+++ i3/.config/i3/scripts/customlock.sh
@@ -1,11 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Config options here : https://github.com/chrjguill/i3lock-color
+image_file=/tmp/screen_lock.png
+image_file_final=/tmp/screen_lock_final.png
+rm $image_file
+rm $image_file_final
 
-i3lock -k --blur 6 --timecolor=FFFFFFD0 \
-	--datecolor=FFFFFFD0 \
-	--insidecolor=FFFFFF00 \
-	--ringcolor=4a2c5bFF \
-	--keyhlcolor=2a1c33FF \
-	--insidevercolor=FFFFFF00 \
-	--ringvercolor=4a2c5bFF
+resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
+# filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor' 
+filters='boxblur=luma_radius=min(h\,w)/100'
+# lock_overlay='-i /home/mabez/.config/i3/icons/circlelockcleardark.png -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2"'
+
+
+ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
+  -vf "$filters" "$image_file"
+
+ffmpeg -y -loglevel 0 -i $image_file -i /home/mabez/.config/i3/icons/locked-padlock.png -filter_complex \
+"[1:v]scale=256:256 [ovrl], \
+[0:v][ovrl]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
+$image_file_final
+
+# ffmpeg -y -s "$resolution" -i "$image_file" $lock_overlay  "$image_file"
+
+i3lock -e -u -i "$image_file_final"
+
+### OLD, slow with convert
+
+# set the icon and a temporary location for the screenshot to be stored
+# icon="$HOME/images/lock-icon-light.png"
+# tmpbg='/tmp/screen.png'
+
+# # take a screenshot
+# scrot "$tmpbg"
+
+# # blur the screenshot by resizing and scaling back up
+# # convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
+# mogrify "$tmpbg" -filter Gaussian -resize %20 -define "filter:signma=1.5" -resize 500.5% "$tmpbg"
+
+# # overlay the icon onto the screenshot
+# # convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
+
+# # lock the screen with the blurred screenshot
+# i3lock -i "$tmpbg" -u
