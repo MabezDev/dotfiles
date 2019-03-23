f05f89811fc76b0b259ee34fb56a5aa62a856665
Scott Mabin <MabezDev@gmail.com>
2019-03-23 01:32:39 +0000

    Fixed nvim alias for laptop, fixed the bt script its now works for any BT device, added keyboard section for my anne key pro

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -2,21 +2,19 @@
 
 CMD=$1
 NAME=$2
-MAC=$(echo -e 'paired-devices' | bluetoothctl | grep "Device" | grep "WH-1000XM2" | cut -d" " -f2)
-# NAME="WH-1000XM2"
-# MAC=$2
-# echo "#1"
+MAC=$(echo -e 'paired-devices' | bluetoothctl | grep "Device" | grep $NAME | cut -d" " -f2)
+
 
 if [[ "$CMD" == "status" ]]; then
-    if [[ $(echo -e 'info' | bluetoothctl | grep "Name: " | cut -d" " -f2) == *"$NAME"* ]]; then
+    if [[ $(echo -e "info $MAC" | bluetoothctl | grep "Connected: " | cut -d" " -f2) == "yes" ]]; then
         echo "ON"
     else
         echo "OFF"
     fi
 elif [[ "$CMD" == "connect" ]]; then
-    if [[ $(echo -e 'info' | bluetoothctl) != *"$NAME"* ]]; then
+    if [[ $(echo -e "info $MAC" | bluetoothctl | grep "Connected: " | cut -d" " -f2) == "no" ]]; then
         $(echo -e "connect $MAC" | bluetoothctl)
     fi
 elif [[ "$CMD" == "disconnect" ]]; then
-    $(echo -e "disconnect" | bluetoothctl)
+    $(echo -e "disconnect $MAC" | bluetoothctl)
 fi
