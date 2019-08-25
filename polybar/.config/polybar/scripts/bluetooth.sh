9e534bf3f7c5603e1f559b1ad5b421e9ac5920cf
Scott Mabin <scott@mabez.dev>
2019-08-25 17:35:39 +0100

    Update bluetooth script

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -3,16 +3,13 @@
 CMD=$1
 NAME=$2
 DEFAULT_CONTROLLER="00:1A:7D:DA:71:07"
-MAC=$(echo -e 'paired-devices' | bluetoothctl | grep "Device" | grep "WH-1000XM2" | cut -d" " -f2)
-# NAME="WH-1000XM2"
-# MAC=$2
-# echo "#1"
+MAC=$(echo -e 'paired-devices' | bluetoothctl | grep "Device" | grep $NAME | cut -d" " -f2)
 
 if [[ "$CMD" == "status" ]]; then
-    if [[ $(echo -e 'info' | bluetoothctl | grep "Name: " | cut -d" " -f2) == *"$NAME"* ]]; then
-        echo "ON"
-    else
+    if [[ $(echo -e "info $MAC" | bluetoothctl | grep "Connected: " | cut -d" " -f2) == "no" ]]; then
         echo "OFF"
+    else
+        echo "ON"
     fi
 elif [[ "$CMD" == "connect" ]]; then
     if [[ $(echo -e 'info' | bluetoothctl) != *"$NAME"* ]]; then
