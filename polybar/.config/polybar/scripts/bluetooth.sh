6b2f4cd95fa242d0f86680b32de155f4c4d95a7c
Scott Mabin <scott@mabez.dev>
2019-08-25 23:15:52 +0100

    Add AP to bluetooth config

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -6,17 +6,17 @@ DEFAULT_CONTROLLER="00:1A:7D:DA:71:07"
 MAC=$(echo -e 'paired-devices' | bluetoothctl | grep "Device" | grep $NAME | cut -d" " -f2)
 
 if [[ "$CMD" == "status" ]]; then
-    if [[ $(echo -e "info $MAC" | bluetoothctl | grep "Connected: " | cut -d" " -f2) == "no" ]]; then
-        echo "OFF"
-    else
+    if [[ $(echo -e "info $MAC" | bluetoothctl | grep "Connected: " | cut -d" " -f2) == "yes" ]]; then
         echo "ON"
+    else
+        echo "OFF"
     fi
 elif [[ "$CMD" == "connect" ]]; then
-    if [[ $(echo -e 'info' | bluetoothctl) != *"$NAME"* ]]; then
+    if [[ $(echo -e "info $MAC" | bluetoothctl | grep "Connected: " | cut -d" " -f2) == "no" ]]; then
         # make sure were connecting from the right controller on my desktop, should can be ignored on other platforms
         $(echo -e "select $DEFAULT_CONTROLLER" | bluetoothctl) 
         $(echo -e "connect $MAC" | bluetoothctl)
     fi
 elif [[ "$CMD" == "disconnect" ]]; then
-    $(echo -e "disconnect" | bluetoothctl)
+    $(echo -e "disconnect $MAC" | bluetoothctl)
 fi
