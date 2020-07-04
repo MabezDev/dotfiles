684fca1fce7d49c5b1574026eded49f0c25ef3a0
Scott Mabin <scott@mabez.dev>
2020-07-04 01:17:44 +0100

    added theme changer, Fix paths once and for all, all paths stored in zshenv now, so non-interactive shells pull in the paths. i3 focus now jumps to that window, instead of alert

---
+++ rofi/.config/rofi/themeswitch.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+### Created by ilsenatorov
+### Change WALLPAPERDIR to the directory where you store wallpapers
+
+WALLPAPERDIR=$HOME/.wallpapers/
+
+if [ -z $@ ]
+then
+function get_themes()
+{
+    ls $WALLPAPERDIR
+}
+echo current; get_themes
+else
+    THEMES=$@
+    if [ x"current" = x"${THEMES}" ]
+    then
+        exit 0
+    elif [ -n "${THEMES}" ]
+    then
+        /home/mabez/.local/bin/wal -i $WALLPAPERDIR${THEMES} > /dev/null
+    fi
+fi
