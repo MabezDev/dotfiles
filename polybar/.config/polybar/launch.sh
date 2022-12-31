a56d2d8bbd1fbf7c5949d28fbcdfbde8da1f993f
Scott Mabin <scott@mabez.dev>
2022-12-31 18:23:04 +0000

    2022 q4

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,9 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar main &
-#polybar bar2 &
 
 PRIMARY=$(xrandr --query | grep " primary" | cut -d" " -f1)
 if type "xrandr"; then
--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -2,11 +2,11 @@
 
 CMD=$1
 NAME=$2
-MAC=$(bluetoothctl paired-devices | grep "Device" | grep $NAME | cut -d" " -f2)
+MAC=$(bluetoothctl devices | grep "Device" | grep $NAME | cut -d" " -f2)
 
-# make sure were connecting from the right controller on my desktop, should can be ignored on other platforms
-# DEFAULT_CONTROLLER="00:1A:7D:DA:71:07"
-# $(bluetoothctl select $DEFAULT_CONTROLLER) 
+# TODO use this to print battery level!
+# Needed experimental = true in /etc/bluetooth/main.conf
+# dbus-send --print-reply=literal --system --dest=org.bluez /org/bluez/hci0/dev_70_26_05_89_9F_28 org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage"
 
 if [[ -z "$MAC" ]]; then
     echo "MISS"
@@ -16,9 +16,11 @@ fi
 if [[ "$CMD" == "status" ]]; then
     if [[ $(bluetoothctl info $MAC | grep "Connected: " | cut -d" " -f2) == "yes" ]]; then
         echo "ON"
+        # echo "%{F#f00}OFF" # TODO set colour based on battery level?
     else
         echo "OFF"
     fi
+
 elif [[ "$CMD" == "connect" ]]; then
     if [[ $(bluetoothctl info $MAC | grep "Connected: " | cut -d" " -f2) == "no" ]]; then
         bluetoothctl connect $MAC
